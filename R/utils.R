b4a48209ec2be06f420f37720722ec5a1e532d0a
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-07-23 23:11:07 +0200

    remove convert_notation function

--- R/utils.R
+++ R/utils.R
@@ -1,49 +1,10 @@
-#' Convert between tooth notations
-#'
-#' Function to switch between tooth notations.
-#' @param data A data frame to convert
-#' @param tooth_cols the column(s) to convert. If the data are in long format,
-#' i.e., one row per tooth, this should be a single column. If the data are in
-#' wide format, this should be a ([dplyr::dplyr_tidy_select]) selection of columns.
-#' @param from string. the tooth notation from the original data.
-#' @param to string. The tooth notation you wish to convert to.
-#' @export
-convert_notation <- function(data, tooth_cols, from, to){
-  # issues: premolar notation (P1 & P2 vs. P3 & P4)
-    # doesn't work if not all teeth are present (at least not the wide form)
-
-  from_vec <- tooth_notation[[from]]
-  to_vec <- tooth_notation[[to]]
-
-  n_vars <- ncol(dplyr::select(data, {{ tooth_cols }})) # there must be a better way...
-  if(n_vars == 1){
-    # rename with mutate
-    names(to_vec) <- from_vec
-    renamed_data <- dplyr::mutate(
-      data,
-      "{tooth_cols}" := recode_vector({{ tooth_cols }}, index = to_vec)
-    )
-  } else {
-    if(from == "FDI"){
-      from_vec <- paste0("T", from_vec)
-      #print(from_vec)
-      data <- dplyr::rename_with(
-        data,
-        \(x) paste0("T", stringr::str_extract(x, "\\d+")),
-        .cols = {{ tooth_cols }}
-      )
-      #print(data)
-    } else if(from == "text"){
-      data <- data
-      #data <- rename_with(data, \(x) stringr::str_to_upper(x), .cols = tooth_cols) # unknown issue here (Error: object 'URM3' not found)
-    }
-    names(from_vec) <- to_vec # create named vector for conversion (from = value, to = name)
-    renamed_data <- rename(data, all_of(from_vec))
-    #  }
-  }
-  return(renamed_data)
+# strip letter prefix from tooth numbers (if present)
+strip_prefix <- function(data, col){
+  dplyr::mutate(
+    data,
+    "{{ col }}" := gsub("[[:alpha:]]+", "", {{ col }})
+  )
 }
-
 
 # convert caries scores using Standards scoring to counts
 standards_count <- function(data, caries, unobs){
